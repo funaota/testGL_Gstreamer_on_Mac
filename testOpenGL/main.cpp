983b8acd0829779207e324c261ec4a4325f15450
takujifunao <takuji.funao@gmail.com>
2016-08-01 14:42:10 +0900

    Initial Commit

---
+++ testOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testOpenGL
+//
+//  Created by takujifunao on 2016/08/01.
+//  Copyright © 2016年 takujifunao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
